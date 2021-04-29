5295e2aec6f9019232eea5f50b910bc59460dd85
P-hansen <philip.pt.hansen@gmail.com>
2021-04-29 23:47:14 +0000

    working create category route

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  
   def index
-    @categories = 5#Category.from(:name)
+    @category_count = Category.count(:name)
+    @category_names = Category.select(:name)
+    @category = Category.new
   end
 
+  # def new
+  #   @category = Category.new
+  # end
+
   def create
-    @category = Category.new(category_name)
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
