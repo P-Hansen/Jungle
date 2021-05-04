982fbe894a32960e831d3d9a66d5332ce5316fd6
P-hansen <philip.pt.hansen@gmail.com>
2021-05-04 23:26:58 +0000

    product click test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      10.times do |n|
+        @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+      end
+    end
+
+    scenario "They can click product details" do
+      # ACT
+      visit root_path
+      # VERIFY
+      expect(page).to have_css 'article.product', count: 10
+      click_on('Details', match: :first)
+      sleep(1)
+      # DEBUG
+      save_screenshot 'test_2.png'
+    end
+
+end
